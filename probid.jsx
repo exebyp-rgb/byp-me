6e16b6a1c1c86adf333ccbe444cd2ace23e26426
exebyp-rgb <exebyp@gmail.com>
2026-03-08 11:23:37 +0200

    Add authentication and quote builder components

---
+++ probid.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ProbId = () => {
+    return (
+        <div>
+            <h1>Quote Builder</h1>
+            {/* Your quote builder logic goes here */}
+        </div>
+    );
+};
+
+export default ProbId;
